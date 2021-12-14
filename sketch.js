76065e0eecc22ad1def08f8a06110d378c5e8c81
CoconutPeanut <92638392+CoconutPeanut@users.noreply.github.com>
2021-12-14 18:57:06 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+
+function setup() {
+  createCanvas(400,400);
+S1 = new Student("Himani", 14);
+S1.show();
+
+S2 = new Student("Himanshi", 12);
+S2.show();
+
+
+}
+
+function draw() 
+{
+  background(30);
+  
+}
---
+++ student.js
@@ -0,0 +1,16 @@
+
+class Student
+{
+constructor(name, age){
+
+    this.name = name
+    this.age = age
+} 
+show(){
+    console.log(this.name);
+    console.log(this.age);
+} 
+}
+
+
+
